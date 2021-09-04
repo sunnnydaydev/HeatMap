fc811e97c215d0ff04c3a237cca4398547b387f7
SunnyDay <7403365+sunnnydaydev@user.noreply.gitee.com>
2021-09-04 15:12:26 +0800

    aMap heat map finish

---
+++ app/src/main/java/com/sunnyday/heatmap/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.sunnyday.heatmap
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.amap.api.maps.AMap
+import com.amap.api.maps.model.HeatMapLayerOptions
+import com.amap.api.maps.model.HeatmapTileProvider
+import com.amap.api.maps.model.LatLng
+import com.amap.api.maps.model.TileOverlayOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+        const val SIZE = 500
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mMapView.onCreate(savedInstanceState)
+
+        val aMap = mMapView.map
+        generateHeatMap(aMap = aMap)
+
+        // todo 热力图修改为指定位置
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mMapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView.onDestroy()
+    }
+
+    /**
+     * generate heat map
+     * */
+    private fun generateHeatMap(aMap: AMap) {
+
+        //1:第一步，组织热力图数据
+        val list = ArrayList<LatLng>()
+        val defaultX = 39.904979
+        val defaultY = 116.40964
+
+        repeat(SIZE) {
+            Log.d(TAG,"index:$it")
+            val x = Math.random() * 0.5 - 0.25
+            val y = Math.random() * 0.5 - 0.25
+            list.add(LatLng(defaultX+x,defaultY+y))
+        }
+
+        //2:构建热力图 HeatMapTileProvider
+        val heatMapTileProvider = HeatmapTileProvider.Builder()
+            .data(list)
+            .gradient(HeatMapLayerOptions.DEFAULT_GRADIENT)
+            .build()
+
+        //3:绘制热力图图层
+        val tileOverlayOptions = TileOverlayOptions() // 初始化 TileOverlayOptions
+        tileOverlayOptions.tileProvider(heatMapTileProvider)// 设置瓦片图层的提供者
+        aMap.addTileOverlay(tileOverlayOptions)// 向地图上添加 TileOverlayOptions 类对象
+    }
+}
